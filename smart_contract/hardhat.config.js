a3d9c85ba6a02372cc3fd95d4bccbd76cd4624dc
Shikhar Bansal <shikharbansal007@gmail.com>
2022-01-11 15:19:18 +0530

    Done with final API integration

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,15 @@
+//https://eth-ropsten.alchemyapi.io/v2/R3-swUVSNsT7AGrenV51CWEl5S3uqOwV
+
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/R3-swUVSNsT7AGrenV51CWEl5S3uqOwV",
+      accounts: [
+        "f715d917c2bd150cb3088b00d9d3bb96093072de6ef31c870346d4e26a0c961a",
+      ],
+    },
+  },
+};
